2e426816af30fe383d70ae94c81d5c1843f5b8df
Drakonqs <kolya.barinov01@mail.ru>
2022-09-06 17:29:43 +0300

    Добавьте файлы проекта.

---
+++ SwordDamege/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordDamege
+{
+    class Program
+    {
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            SwordDamage swordDamage = new SwordDamage(RollDice());
+            while (true)
+            {
+                Console.Write("Нажатие 0 - меч обычный, 1 - магический, 2 - огненый, " + "3 - и то и то, любая другая, выход: ");
+                char key = Console.ReadKey().KeyChar;
+                if (key != '0' && key != '1' && key != '2' && key != '3') return;
+                swordDamage.Roll = RollDice();
+                swordDamage.Magic = (key == '1' || key == '3');
+                swordDamage.Flaming = (key == '2' || key == '3');
+                Console.WriteLine($"\nБросок  {swordDamage.Roll} наносит повреждений {swordDamage.Damage} HP\n");
+            }
+        }
+        private static int RollDice()//Бросок кубика
+        {
+            return random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+        }
+    }
+}
+//static void Main(string[] args)
+//{
+//    while (true)
+//    {
+//        Random random = new Random();
+//        SwordDamage swordDamage = new SwordDamage();
+//        Console.Write("Нажатие 0 - меч обычный, 1 - магический, 2 - огненый, " + "3 - и то и то, любая другая, выход: ");
+//        char key = Console.ReadKey().KeyChar;
+//        if (key != '0' && key != '1' && key != '2' && key != '3') return;
+//        int roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+//        swordDamage.Roll = roll;
+//        swordDamage.SetMagic(key == '1' || key == '3');
+//        swordDamage.SetFlaming(key == '2' || key == '3');
+//        Console.WriteLine("\nБросок " + roll + " наносит повреждений " + swordDamage.Damage + " HP\n");
+
+//    }
+
+//}
---
+++ SwordDamege/SwordDamage.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordDamege
+{
+    class SwordDamage
+    {
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+        /// <summary>
+        /// Содержит расчетный урон.
+        /// </summary>
+        public int Damage { get; private set; }
+        private int roll;
+        /// <summary>
+        /// Устанавливает или получает бросок 3d6.
+        /// </summary>
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+        private bool magic;
+        /// <summary>
+        /// True, если меч волшебный; false в противном случае.
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+        private bool flaming;
+        /// <summary>
+        /// True, если меч огненный; false в противном случае.
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+        /// <summary>
+        /// Вычисляет повреждения в зависимости от текущих значений свойств.
+        /// </summary>
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic) magicMultiplier = 1.75M;
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming) Damage += FLAME_DAMAGE;
+        }
+        /// <summary>
+        /// Конструктор вычисляет повреждения для значений Magic и Flaming по умолчанию 
+        /// и начального броска 3d6.
+        /// </summary>
+        /// <param name="startingRoll">Начальный бросок 3d6</param>
+        public SwordDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+    }
+}
+//class SwordDamage
+    //{
+    //    public const int BASE_DAMAGE = 3;
+    //    public const int FLAME_DAMAGE = 2;
+    //    public int Roll;
+    //    public decimal MagicMultiplier = 1M;
+
+    //    public int Damage;
+    //    public void CalculateDamage()//Считаем урон мечём
+    //    {
+    //        Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE;
+    //    }
+    //    public void SetMagic(bool isMagic)//Магический или нет
+    //    {
+    //        if (isMagic)
+    //        {
+    //            MagicMultiplier = 1.75M;
+    //        }
+    //        else
+    //        {
+    //            MagicMultiplier = 1M;
+    //        }
+    //        CalculateDamage();
+    //    }
+    //    public void SetFlaming(bool isFlaming)//Огненый или нет
+    //    {
+    //        CalculateDamage();
+    //        if (isFlaming)
+    //        {
+    //            Damage += FLAME_DAMAGE;
+    //        }
+    //    }
